513d557d86baea9c84708d07ccdb03b1cb1e6a69
canhhandsome <cbd1708@gmail.com>
2025-04-16 11:35:18 +0700

    feat: add customer sidebar layout with navigation and logout button

---
+++ src/components/ButtonSidebar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function ButtonSidebar({ icon, label, link,type="normal" }) {
+  const normalSyle = `bg-white border border-gray-200 flex gap-4 p-8 py-3 rounded-lg text-black font-medium text-2xl hover:bg-gray-100`;
+  const logOutStyle = `w-full border-2 border-white text-white rounded-lg font-semibold py-2 px-4 flex gap-2 items-center justify-center text-2xl hover:bg-white hover:text-[#96C576] transition duration-300`
+  return (
+    <Link to={`/${link}`} className={type === "logout" ? logOutStyle : normalSyle}>
+        {icon}
+        {label}
+    </Link>
+  );
+}
