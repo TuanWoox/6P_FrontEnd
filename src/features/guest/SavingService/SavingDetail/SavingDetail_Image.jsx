7c0e1e8ffd46468123b39f148d0da6f549c3abaa
ttoann <ntt26.5.2004@gmail.com>
2025-04-29 15:47:30 +0700

    Modify UI(Update the images URL, change theme) of View Service and View Service Detail(Saving) use case.

--- src/features/guest/SavingService/SavingDetail/SavingDetail_Image.jsx
+++ src/features/guest/SavingService/SavingDetail/SavingDetail_Image.jsx
@@ -13,11 +13,11 @@ function SavingDetail_Image({
         <div className="w-full h-[550px] bg-cover bg-center relative p-24 rounded-2xl drop-shadow-lg"
             style={{
                 backgroundImage:
-                    "url('https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/San-pham-Dich-vu/Tiet-kiem/DANH-SACH-_-CHI-TIET_TIET-KIEM/HBB_Tiet-kiem-tra-lai-sau_DT.jpg?h=1500&iar=0&w=3936&ts=20230813024133&hash=35AA2EEAE01E5665AB598D68DC85FB36')"
+                    "url('https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/San-pham-Dich-vu/Tiet-kiem/DANH-SACH-_-CHI-TIET_TIET-KIEM/HBB_RUT-GOC-LINH-HOAT_DT.jpg?h=1500&iar=0&w=3936&ts=20230813070200&hash=4EFE82DFF8ED3326B366E70ADAA7F421')"
             }
             }>
             <div className="mt-16">
-                <p className="text-green-800 font-semibold text-4xl">
+                <p className="text-[#95C475] font-semibold text-4xl">
                     {title}
                 </p>
                 <div className="flex">
--- src/features/guest/SavingService/SavingDetail/savingDetailData.js
+++ src/features/guest/SavingService/SavingDetail/savingDetailData.js
@@ -14,17 +14,17 @@ const dataForTermDeposit = {
         {
             title: "Loại tiền gửi đa dạng",
             content: "Gửi tiền bằng nhiều loại ngoại tệ khác nhau: USD, EUR, GBP, AUD, v.v.",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/c340/42bc/50a7eea6fcaca63b9b937ff3fc2d8cf7?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=L7nTZlb5Zyt9HXGbSrDW3wuhHiQRVamL11dKK9-2C5NSwAC9MWwaRtzAvBxsyww~c~MUxG76~4c7HIPkaMPip8dGMUSCp5j4mrbdQOgN8GTUYX79dFxLY07ycZzVTQDbt0u8y7ivggsi5msZsQVhc5Q4MVLNO6~B3zpKe0UXuBu1K9-T6x9YsLFg8tuCN9qM1zGkQ4adqqXCHSaqotTJKZNKR3Jo15jb3Z6VHkxLkcOcJ8m3P2eIAE7VpztewEmQpmLKxGvLnQlvW9I912obwGzfwdHy0wM~8blr7WAxOJpjsD61ibyALFxx3hbYVRpjSzrF6g4XtRc5KzdNMg5GJQ__",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/San-pham-Dich-vu/Tiet-kiem/Icon-USP/loai-tien-gui-da-dang.svg?iar=0&ts=20230428092907&hash=8DD49378B0B7FC6A367DA8F9B11B8195",
         },
         {
             title: "Kỳ hạn linh hoạt",
             content: "Từ tối thiểu 1 tháng đến tối đa 24 tháng",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/d88c/dc47/f808ff6a9bdb700fbb23ea1e7ee6eedc?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=FYdx-Zf576P2gqfFIfuIS0cihSbc04jLl8Iijn~Z9a9xd9Lj0006mgg3K89N7Cik-HGA4CmApeRpxdIv4yorg55P1Ikujz8HN~Pynv0XN~T153vs5BRX0NecvFRcdJMG1EpLIz5eIUIe~apzXIe6W-SxrSXqPSlmWg5WcPb54RfS7IxRsUK1gM1-CFQBRYWT5~0cb64d~MnX9Fm7R7kMy0AUxl9HFruaxq5W-lOM7nu77-6UoFd5XCz~qbV2BbKvle7oSZT25TzDfWKAL7ydaabCd-CRlwLdkjruBuZbR-IY-TdFGpfJD9avkUvkcHhOY6VK99pFjoJuDNZWapa0WA__",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/San-pham-Dich-vu/Tiet-kiem/Icon-USP/ky-han-phong-phu.svg?iar=0&ts=20230428092907&hash=07CD354026FE95C6D7A82FD40D27AC30",
         },
         {
             title: "Thủ tục nhanh chóng",
             content: "Gửi và rút dễ dàng tại tất cả điểm giao dịch của 6PBank trên toàn quốc",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/0349/2017/d7385cd41888378bb443ea48107757d8?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=KnjsH79FOAAVNLqqcEDLRmNZWvBQQTrKoCl7cinI8FRIce2s~VTUj~vDE6JUfifnVzzPISl-SIPVtk0j-X72dBLITqSF4AW1n260Btc7hGYihC6UgzLgUg2EDzttVaXHdfyk8g1frPIq9cDJJdSlGOSHIfdibfamGU~~23P1yrJPpqaif0TEF4dsulvWe9TCGhJD~pyNpxDgTFMyPJvsePMmmH~0QkiNfEVtu1pa6CR3jsIsjsb5OxS0pCe8bRAs9XDxRmC8MAVTzWPrq8uIeevS9TMrxJDaMl9YrQg8TNotOyRfKtYmANW-GWp74q2rbjMryYsR6WqmMOY7Qn7AKw__",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/San-pham-Dich-vu/Tiet-kiem/Icon-USP/thu-tuc-nhanh-chong.svg?iar=0&ts=20230428092906&hash=8B3DBEECA1E19CCBDCAF32EF4EBE13E3",
         }
     ],
 
@@ -78,17 +78,17 @@ const dataForDemandDeposit = {
         {
             title: "Tối ưu lợi ích",
             content: "Giữ nguyên lãi suất khoản tiền còn lại, hưởng lãi suất không kỳ hạn khoản tiền rút trước hạn",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/8df7/af8c/86feb78f829a19f1457ccabbe1c5d4cd?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=by6qUT8xk2NM1mL5IEiuKkr~NfnN5CJUaYkQwp5SagocFGsBKrjunjy9Ynvui4XbUzfW~9k6o9EBTtQoDG1wAvj7U0Rf~uillZuOIfOyuxH2g0RyLulA4qwDGuYhU9m8eBqm25VeixaEa8I8MQdTc2fHRmvOdVVx3fQsLxeTOs2v5AkKuwxS269~wzb7LlimMkKvbwqDtuQBIHL-B-D8UN2kpc-n1YD~VcGYaYa0cufc71D8vG-XINOSJxUWuQvGPFP0wFmnwZA6vGoEFAxxxHzoc~M3Q6Ak5428z9b-3vmZDWgsmtTwbr-xgg85uDsuc1xxKMcFZCgH5U~h2jX3jg__",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/Lien-he-va-Ho-tro/HDSD/Icon-update-2105/Tiet-kiem/patch-check.svg?iar=0&ts=20230331094739&hash=ECFEAAF648C99437C53826442DBCBE57",
         },
         {
             title: "Rút gốc linh hoạt",
             content: "Rút một phần tiền gửi mà không phải tất toán toàn bộ",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/098b/a6aa/97343edd1e9b73294b0e63ba62ffee9d?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=nGHQ2ab0UmLyghhB-L-wGN~RrJwyN5YRAah6YyQD1xFzep3gliBvwMDicniyxLP01~cuCN96ewJ6WodjHDScQ51i~oOuyoxCLeq5FmMQzJpyDZnrZwiaqSYZCfVIrYgEgM4PPzZAFsQ1~Q96WPUT-ONlHSoBMo~xQHLxX0hfHzvHI1G0GqbMCtqK7PgC6VUaeVg2NHHYV-RZn25v2FOoVwHhv1sj02njiHGC896S3lENVzt9nIi2g8eeBNqUgRlrPmLdAjmk1uiHRJ0c9Z23OHJ5SlvE0igzslcXpdAA1oZ9PXcQQSVTDjDWdtOFhQ3Q0haz7lGYTIX0LaCkPhxz6A__",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/Lien-he-va-Ho-tro/HDSD/Icon-update-2105/Tiet-kiem/money-recive.svg?iar=0&ts=20230331094730&hash=0614BF7E187DBE33B78C812FAB8DAD16",
         },
         {
             title: "Giao dịch thuận tiện",
-            content: "Gửi và rút dễ dàng qua Ngân hàng số 6P Digibank hoặc điểm giao dịch của 6P Bank",
-            imgSrc: "https://s3-alpha-sig.figma.com/img/ef18/54ea/4c1f4e3db0bbd9066de112f37ffca00d?Expires=1745193600&Key-Pair-Id=APKAQ4GOSFWCW27IBOMQ&Signature=k224RxJcNZ5ySZ3cSIJlC1uJZbZwIdqNuxnZjsVvdeuTa-oUlCA5lgLUidUnC84vZrHCAIQcfrIOYCS2XiTcBA~mc83bedh8LlUle2s4of9CZd6EDAlKsVXE7Bp6fuX6SKOmaYHEgU361J5pCk~jg6YpzAZtXdOyiGacrjdGNsDmjr2xnCQ4mNexGwMTTT5dozxl-fYYObEld6QgBBkNMUOyFwZBfav8szPX5RY~ugle9s7uBGXVS8Me7mGI1NUwTidAa78XghiFzHjAoFSD9unZw82r1CYl31gRTDZEuW5UXK0gmHfk89QKLGH2QUtEDTLnhYBzOR-Icix~wdQD5g__",
+            content: "Gửi và rút dễ dàng qua Ngân hàng số 6P Bank hoặc điểm giao dịch của 6P Bank",
+            imgSrc: "https://www.vietcombank.com.vn/-/media/Project/VCB-Sites/VCB/KHCN/Icon-update-1307/calendar-tick.svg?iar=0&ts=20230713025834&hash=CA4E59856C8DD5D0CC486DD9BCDCB61A",
         }
     ],
 
