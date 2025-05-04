b0fee274e895ab8dd524d44bf01cedf3e1f10e6c
Tran Trung Tin <trantrungtin254@gmail.com>
2025-05-04 16:27:23 +0700

    LoanPayment: Add queryClient import and invalidate sidebarInfo query after loan payment confirmation

--- src/hooks/useLoanPayment.js
+++ src/hooks/useLoanPayment.js
@@ -1,5 +1,6 @@
 import { useMutation } from "@tanstack/react-query";
 import axiosAuth from "../axios/axios";
+import queryClient from "../config/reactQuery";
 
 const API_URL = import.meta.env.VITE_BACKEND_URL;
 
@@ -10,6 +11,9 @@ export function useLoanPayment() {
                 `${API_URL}/loanAccount/loanPayment/confirm`,
                 paymentData,
             );
+            queryClient.invalidateQueries({
+                queryKey: ["sidebarInfo"],
+            });
             return response.data;
         } catch (error) {
             const errMsg =
